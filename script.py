6780afc9de978921637e287eb82adcc56c140073
unknown <peter7321@gmail.com>
2019-12-17 21:17:34 +0100

    remove some code

--- script.py
+++ script.py
@@ -1,15 +1,6 @@
-import sys
 import requests
 
-#print(sys.version)
-print(sys.executable)
 
-def greet(who_to_greet):
-    greeting = 'Hello, {}'.format(who_to_greet)
-    return greeting
-
-r = requests.get('https://coreyms.com')
+r = requests.get("https://coreyms.com")
 print(r.status_code)
-
-#print(greet('World'))
-#print(greet('Peter'))
+print(r.ok)
